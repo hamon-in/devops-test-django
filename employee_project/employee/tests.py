c977c4e8760a61a3b0804718d351891f7afe22f4
Suroor Hussain <suroorhussain@hamon.in>
2024-07-22 19:45:19 +0530

    Finish test backend

--- employee_project/employee/tests.py
+++ employee_project/employee/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from faker import Faker
+from .models import Employee
 
-# Create your tests here.
+class EmployeeTests(APITestCase):
+    def setUp(self):
+        # Initialize Faker instance
+        self.fake = Faker()
+        # Clear the database
+        Employee.objects.all().delete()
+        self.list_url = reverse('employee-list-create')
+        self.detail_url = lambda pk: reverse('employee-detail', kwargs={'pk': pk})
+
+    def create_employee(self):
+        # Generate new random employee data
+        employee_data = {
+            'first_name': self.fake.first_name(),
+            'last_name': self.fake.last_name(),
+            'email': self.fake.unique.email(),
+            'job_title': self.fake.job(),
+            'date_of_birth': self.fake.date_of_birth(minimum_age=18, maximum_age=65).isoformat()
+        }
+        return Employee.objects.create(**employee_data), employee_data
+
+    def test_get_employee_list(self):
+        # Create an employee
+        self.create_employee()
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)  # Assuming we have one employee
+
+    def test_get_employee_detail(self):
+        # Create an employee
+        employee, employee_data = self.create_employee()
+        response = self.client.get(self.detail_url(employee.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], employee_data['email'])
+
+    def test_create_employee(self):
+        # Generate new random data for creating an employee
+        employee_data = {
+            'first_name': self.fake.first_name(),
+            'last_name': self.fake.last_name(),
+            'email': self.fake.unique.email(),
+            'job_title': self.fake.job(),
+            'date_of_birth': self.fake.date_of_birth(minimum_age=18, maximum_age=65).isoformat()
+        }
+        response = self.client.post(self.list_url, employee_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Employee.objects.count(), 1)  # Only one created here
+        self.assertEqual(Employee.objects.get(id=response.data['id']).email, employee_data['email'])
--- employee_project/employee/views.py
+++ employee_project/employee/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Employee
+from .serializers import EmployeeSerializer
 
-# Create your views here.
+class EmployeeListCreate(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class EmployeeDetail(generics.RetrieveAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
